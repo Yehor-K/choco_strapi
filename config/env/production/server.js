fce654b29b4dd8a2747850fcd1459664ce27a815
Yehor-K <yehorkuch@gmail.com>
2021-07-31 16:06:59 +0300

    add admin jwt secret to production env

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,8 @@
 module.exports = ({ env }) => ({
   url: env("https://protected-depths-56006.herokuapp.com/"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "eb58cf382241d084697a4232539998ef"),
+    },
+  },
 });
